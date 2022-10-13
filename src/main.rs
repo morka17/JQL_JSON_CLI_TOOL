afcb013cf103f12b01e89dd1b8f855163a1631cb
morka17 <joshuamorka19@gmail.com>
2022-10-13 14:26:30 -0700

    add the json reference comment

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+
+/*
+JSON quick reference:
+
+string escapes:
+ \* \\ \/
+ \n, \r, \t,
+ \b, backspace, 0x08
+ \f, form feed, 0x0C
+ \uXXXX
+
+ number: -?[0-9]+(,[0-9]+)?([eE][-+]?[0-9]+)?
+
+*/
+
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
