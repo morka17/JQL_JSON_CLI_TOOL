0aa672df5438c819155f099c5f5933077133c9da
morka17 <joshuamorka19@gmail.com>
2022-10-14 02:43:41 -0700

    create the parse null function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate nom;
+
+use nom::{bytes::complete::tag, combinator::value,  IResult, Parser, error::ParseError};
+
 
 /*
 JSON quick reference:
@@ -8,8 +12,6 @@ string escapes:
  \b, backspace, 0x08
  \f, form feed, 0x0C
  \uXXXX
- 
-
  number: -?[0-9]+(,[0-9]+)?([eE][-+]?[0-9]+)?
 
 
@@ -17,6 +19,13 @@ string escapes:
 
 
 
+fn parse_null<'i, E: ParseError<&'i str>>(input: &'i str) -> IResult<&str, (), E>{
+    value((), tag( "null")).parse(input) 
+}
+
+fn parse_bool<'i,  E: ParseError<&'i str>>(input: &'i str) -> IResult<&str, (), E>{
+     
+}
 
 
 fn main() {
